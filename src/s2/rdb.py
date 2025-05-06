f846a84714a6f0fe8e6c087957c0bf10af0cac88
Mariah Gomes <mariah.gomes06@gmail.com>
2025-05-06 15:24:14 -0300

    Tentando mensageria

--- src/s2/rdb.py
+++ src/s2/rdb.py
@@ -1,5 +1,12 @@
 from src.s2.connection import supabase
 
 def inserir_dado_medico(dados):
-    resultado = supabase.table("medico").insert(dados).execute()
-    return resultado
+    try:
+        response = supabase.table('medicos').insert(dados).execute()
+        if response.status_code == 201:  # Se a inserção for bem-sucedida
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Erro ao inserir dado no banco de dados: {e}")
+        return False
