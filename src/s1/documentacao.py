078080112e605fa200414b1e0da2e93b0c9829a0
Iago Rosa <iagoimportantthings@gmail.com>
2025-05-28 19:42:17 -0300

    removendo prints de controle, do código

--- src/s1/documentacao.py
+++ src/s1/documentacao.py
@@ -32,7 +32,6 @@ def documentar_laudo():
         "crm" : crm
     }
     resposta = enviar_mensagem_aguardando('buscar_ids', dados)
-    print(resposta.get("mensagem"))
     if resposta["resultado"]:
         ids = resposta.get("mensagem")
         data = input("Digite a data do laudo (YYYY-MM-DD): ")
@@ -44,7 +43,6 @@ def documentar_laudo():
             "prescricao" : prescricao
         }
         resposta_documentar = enviar_mensagem_aguardando("documentar_laudo", documentar)
-        print(resposta_documentar["mensagem"])
     else:
         return
     
@@ -54,7 +52,6 @@ def buscar_laudo():
         "cpf" : cpf,
     }
     resposta = enviar_mensagem_aguardando('buscar_idPaciente', dados)
-    print(resposta.get("mensagem"))
     if resposta["resultado"]:
         id = resposta.get("mensagem")
         data = input("Digite a data do laudo (YYYY-MM-DD): ")
@@ -63,7 +60,6 @@ def buscar_laudo():
             "data" : data,
         }
         resposta_busca = enviar_mensagem_aguardando("buscar_laudo", buscar)
-        print(resposta_busca["mensagem"])
     else:
         return
 
@@ -73,7 +69,6 @@ def registrar_exame():
         "cpf" : cpf
     }
     resposta = enviar_mensagem_aguardando('buscar_idPaciente', dados)
-    print(resposta.get("mensagem"))
     if resposta["resultado"]:
         id = resposta.get("mensagem")
         tipo_exame = input("Digite o tipo de exame feito: ")
@@ -88,7 +83,6 @@ def registrar_exame():
             "percentual_aceitacao" : percentual_aceitacao
         }
         resposta_registrar = enviar_mensagem_aguardando("registrar_exame", registrar)
-        print(resposta_registrar["mensagem"])
     else:
         return
     
@@ -98,7 +92,6 @@ def buscar_exame():
         "cpf" : cpf,
     }
     resposta = enviar_mensagem_aguardando('buscar_idPaciente', dados)
-    print(resposta.get("mensagem"))
     if resposta["resultado"]:
         id = resposta.get("mensagem")
         data = input("Digite a data do exame (YYYY-MM-DD): ")
@@ -107,6 +100,5 @@ def buscar_exame():
             "data" : data,
         }
         resposta_busca = enviar_mensagem_aguardando("buscar_exame", buscar)
-        print(resposta_busca["mensagem"])
     else:
         return
